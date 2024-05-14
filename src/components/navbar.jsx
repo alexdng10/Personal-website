3052520402663a4dac96757753869dc4cb2a1549
alexdng10 <154635389+alexdng10@users.noreply.github.com>
2024-05-13 22:36:22 -0400

    added navbar titles

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,6 +1,16 @@
-import Link from "next/link"
+"use client"
 
+
+import Link from "next/link"
+import { useState } from "react";
+const links = [
+    { url: "/", title: "Home"},
+    { url: "/about", title: "About"},
+    { url: "/portfolio", title: "Portfolio"},
+    { url: "/contact", title: "Contact"},
+];
 const Navbar = () => {
+    const [open,setOpen]=useState(false)
     return (
         
         <div className='h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48'>
@@ -13,14 +23,20 @@ const Navbar = () => {
             </div>
             {/* Menu Button */}
             <div className=''>
-                <button className="w-10 h-8 flex flex-col justify-between">
-                    <div className="w-10 h-1 bg-black rounded"></div>
-                    <div className="w-10 h-1 bg-black rounded"></div>
-                    <div className="w-10 h-1 bg-black rounded"></div>
+                <button className="w-10 h-8 flex flex-col justify-between z-50 relative">
+                    <div className="w-10 h-1 bg-white rounded"></div>
+                    <div className="w-10 h-1 bg-white rounded"></div>
+                    <div className="w-10 h-1 bg-white rounded"></div>
                     
                 </button>
+                 {/*MENU LIST*/}
+                    <div className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl">
+                        {links.map(link=>(
+                            <Link href={link.url} key={link.title}>{link.title}</Link>
+                        ))}
+                 </div>
             </div>
-            </div>
+        </div>
     )
 }
 export default Navbar
