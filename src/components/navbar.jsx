cb2dacc0eff3ce8680cc372ceaa90ca549296e7d
alexdng10 <154635389+alexdng10@users.noreply.github.com>
2024-05-13 22:42:23 -0400

    added on click state

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -23,18 +23,23 @@ const Navbar = () => {
             </div>
             {/* Menu Button */}
             <div className=''>
-                <button className="w-10 h-8 flex flex-col justify-between z-50 relative">
+                <button className="w-10 h-8 flex flex-col justify-between z-50 relative " 
+                onClick={(()=>setOpen(prev=>!prev))}>
                     <div className="w-10 h-1 bg-white rounded"></div>
                     <div className="w-10 h-1 bg-white rounded"></div>
                     <div className="w-10 h-1 bg-white rounded"></div>
                     
                 </button>
                  {/*MENU LIST*/}
+                 {open &&(
+
+                 
                     <div className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl">
                         {links.map(link=>(
                             <Link href={link.url} key={link.title}>{link.title}</Link>
                         ))}
                  </div>
+                )}   
             </div>
         </div>
     )
