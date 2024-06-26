854b7629296be851252d02d77afa1a63446e42c7
alexdng10 <hungqdang0510@gmail.com>
2024-06-26 17:36:44 +0700

    working transition

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,34 +1,48 @@
 "use client";
 
-import React, { useContext } from 'react';
+import React, { useState, useContext } from 'react';
+import { motion, AnimatePresence } from "framer-motion";
 import Spline from '@splinetool/react-spline';
 import { MenuContext } from "@/components/menucontext";
 import LoadingScreen from '@/components/LoadingScreen';
 
 export default function HomePage() {
+  const [loading, setLoading] = useState(true);
   const { isMenuOpen } = useContext(MenuContext);
 
+  const handleAnimationComplete = () => {
+    setLoading(false);
+  };
+
   return (
-    
-    <div className="flex flex-col md:flex-row justify-center items-center h-full w-full">
-      <LoadingScreen/>
-      <div className={`flex-1 flex justify-center items-center md:pr-5 h-full ${isMenuOpen ? 'hidden' : 'flex'}`}>
-        {/* Spline for mobile screens */}
-        <div className="w-full max-w-xs md:hidden transform translate-x-[-15%]">
-          <Spline scene="https://prod.spline.design/uJe5uzGCeuqHvySq/scene.splinecode" />
+    <div className="h-full w-full">
+      <AnimatePresence>
+        {loading && <LoadingScreen onAnimationComplete={handleAnimationComplete} />}
+      </AnimatePresence>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: loading ? 0 : 1 }}
+        transition={{ duration: 1 }}
+        className="flex flex-col md:flex-row justify-center items-center h-full w-full"
+      >
+        <div className={`flex-1 flex justify-center items-center md:pr-5 h-full ${isMenuOpen ? 'hidden' : 'flex'}`}>
+          {/* Spline for mobile screens */}
+          <div className="w-full max-w-xs md:hidden transform translate-x-[-15%]">
+            <Spline scene="https://prod.spline.design/uJe5uzGCeuqHvySq/scene.splinecode" />
+          </div>
+          {/* Spline for larger screens */}
+          <div className="hidden md:block w-full max-w-none md:w-auto">
+            <Spline scene="https://prod.spline.design/2RB6hoC36LKNxg43/scene.splinecode" />
+          </div>
         </div>
-        {/* Spline for larger screens */}
-        <div className="hidden md:block w-full max-w-none md:w-auto">
-          <Spline scene="https://prod.spline.design/2RB6hoC36LKNxg43/scene.splinecode" />
+        <div className="flex-1 text-center md:text-left text-white flex flex-col justify-center items-center md:items-start md:pl-5 mt-10 md:mt-0">
+          <h1 className="text-4xl font-bold mb-4">Welcome to Alex Dang&apos;s Portfolio</h1>
+          <p className="text-lg">
+            Alex Dang is a passionate software engineer dedicated to delivering high-quality products.
+            Explore his portfolio to learn more about his projects and expertise.
+          </p>
         </div>
-      </div>
-      <div className="flex-1 text-center md:text-left text-white flex flex-col justify-center items-center md:items-start md:pl-5 mt-10 md:mt-0">
-        <h1 className="text-4xl font-bold mb-4">Welcome to Alex Dang&apos;s Portfolio</h1>
-        <p className="text-lg">
-          Alex Dang is a passionate software engineer dedicated to delivering high-quality products.
-          Explore his portfolio to learn more about his projects and expertise.
-        </p>
-      </div>
+      </motion.div>
     </div>
   );
 }
--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -1,94 +1,127 @@
-// src/components/LoadingScreen.js
-import React from 'react';
-import { motion, AnimatePresence } from "framer-motion";
+import React, { useEffect } from 'react';
+import { motion } from "framer-motion";
 
-
-const LoadingScreen = () => {
+const LoadingScreen = ({ onAnimationComplete }) => {
   const topVariants = {
     action: {
-      x:45,
-      transition:{
-        duration:3
-      }
+      x: 45,
+      transition: {
+        duration: 3,
+      },
     },
-   
-    action2:{
-      rotate:60,
-      scale:2,
-      backgroundColor: "rgb(0,0,255)",
-      transition:{
-        duration:2,
-        delay: 3 
-      }
+    action2: {
+      rotate: 60,
+      scale: 2,
+      backgroundColor: "rgb(0, 0, 255)",
+      transition: {
+        duration: 2,
+        delay: 3,
+      },
     },
     opened: {
       x: "70vw",
     },
-    
   };
+
   const middle = {
     check: {
-      x:0,
-      transition:{
-        duration:2,
-        delay:2
-      }
+      x: 0,
+      transition: {
+        duration: 2,
+        delay: 2,
+      },
     },
-    second:{
-      scale:2,
-      backgroundColor: "rgb(0,0,255)",
-      transition:{
-        duration:2.5,
-        delay:4
-      }
+    second: {
+      scale: 2,
+      backgroundColor: "rgb(0, 0, 255)",
+      transition: {
+        duration: 2.5,
+        delay: 4,
+      },
     },
     opened: {
       x: "70vw",
     },
   };
+
   const bottomVariants = {
     action: {
-      x:-45,
-      transition:{
-        duration:3
-      }
+      x: -45,
+      transition: {
+        duration: 3,
+      },
     },
-   
-    action2:{
-      rotate:-60,
-      backgroundColor: "rgb(0,0,255)",
-      scale:2,
-      transition:{
-        duration:2,
-        delay: 3 
-      }
+    action2: {
+      rotate: -60,
+      backgroundColor: "rgb(0, 0, 255)",
+      scale: 2,
+      transition: {
+        duration: 2,
+        delay: 3,
+      },
     },
     opened: {
       x: "-70vw",
       y: "-32px",
     },
-    
   };
-  const circleChan ={
-      action:{
-        y:-20,
-        scale:100,
-        transition:{
-          duration:3.5,
-          delay: 6.5 
-        }
+
+  const circleChan = {
+    action: {
+      y: -20,
+      scale: 100,
+      transition: {
+        duration: 3.5,
+        delay: 6.5,
       },
-      opened: {
-        y: "450px",
+    },
+    action2: {
+      y: -20,
+      scale: 100,
+      transition: {
+        duration: 2.5,
+        delay: 6.5,
       },
+    },
+    opened: {
+      y: "450px",
+    },
   };
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      onAnimationComplete();
+    }, 7800); // Adjust the timing to match the total duration of your animations
+    return () => clearTimeout(timeout);
+  }, [onAnimationComplete]);
+
   return (
     <div className="fixed inset-0 bg-black flex justify-center items-center z-50">
       <div className="space-y-2 flex flex-col items-center">
-        <motion.div variants={topVariants} initial="opened" animate={["action", "action2"]}   className=" h-2 w-32 bg-white "></motion.div>
-        <motion.div variants={middle} initial="opened" animate={["check","second"]} className=" h-2 w-20 bg-white"></motion.div>
-        <motion.div variants={bottomVariants} initial="opened" animate={["action", "action2"]} className=" h-2 w-32 bg-white"></motion.div>
-        <motion.div variants={circleChan} initial="opened" animate={"action"} className=" h-8 w-8 bg-white rounded-full "></motion.div>
+        <motion.div
+          variants={topVariants}
+          initial="opened"
+          animate={["action", "action2"]}
+          className="h-2 w-32 bg-white"
+        ></motion.div>
+        <motion.div
+          variants={middle}
+          initial="opened"
+          animate={["check", "second"]}
+          className="h-2 w-20 bg-white"
+        ></motion.div>
+        <motion.div
+          variants={bottomVariants}
+          initial="opened"
+          animate={["action", "action2"]}
+          className="h-2 w-32 bg-white"
+        ></motion.div>
+        <motion.div
+          variants={circleChan}
+          initial="opened"
+          animate={"action"}
+          className="h-8 w-8 bg-white rounded-full"
+        ></motion.div>
       </div>
     </div>
   );
