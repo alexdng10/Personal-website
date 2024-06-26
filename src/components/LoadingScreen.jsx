cb6e5d201d0ada83b9e64f943c8fefc65ed0ce10
alexdng10 <hungqdang0510@gmail.com>
2024-06-26 15:02:29 +0700

    added transition effect for landing page

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -6,7 +6,7 @@ import { motion, AnimatePresence } from "framer-motion";
 const LoadingScreen = () => {
   const topVariants = {
     action: {
-      x:55,
+      x:45,
       transition:{
         duration:3
       }
@@ -28,7 +28,7 @@ const LoadingScreen = () => {
   };
   const middle = {
     check: {
-      x:10,
+      x:0,
       transition:{
         duration:2,
         delay:2
@@ -48,7 +48,7 @@ const LoadingScreen = () => {
   };
   const bottomVariants = {
     action: {
-      x:-35,
+      x:-45,
       transition:{
         duration:3
       }
@@ -69,13 +69,26 @@ const LoadingScreen = () => {
     },
     
   };
+  const circleChan ={
+      action:{
+        y:-20,
+        scale:100,
+        transition:{
+          duration:3.5,
+          delay: 6.5 
+        }
+      },
+      opened: {
+        y: "450px",
+      },
+  };
   return (
     <div className="fixed inset-0 bg-black flex justify-center items-center z-50">
       <div className="space-y-2 flex flex-col items-center">
         <motion.div variants={topVariants} initial="opened" animate={["action", "action2"]}   className=" h-2 w-32 bg-white "></motion.div>
         <motion.div variants={middle} initial="opened" animate={["check","second"]} className=" h-2 w-20 bg-white"></motion.div>
         <motion.div variants={bottomVariants} initial="opened" animate={["action", "action2"]} className=" h-2 w-32 bg-white"></motion.div>
-        
+        <motion.div variants={circleChan} initial="opened" animate={"action"} className=" h-8 w-8 bg-white rounded-full "></motion.div>
       </div>
     </div>
   );
